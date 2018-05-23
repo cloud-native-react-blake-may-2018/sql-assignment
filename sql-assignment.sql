da0850d5738c3be2f7c26a02913fe93fbc20490e
Eric Morrison <malin1@protonmail.ch>
2018-05-23 14:21:33 -0400

    finished delete query

--- sql-assignment.sql
+++ sql-assignment.sql
@@ -65,35 +65,53 @@ select * from invoice where billingaddress like 'T%';
 
 --2.6 BETWEEN
 --Task – Select all invoices that have a total between 15 and 50
---Task – Select all employees hired between 1st of June 2003 and 1st of March 2004
+select * from invoice where total between 15 and 50;
 
+--Task – Select all employees hired between 1st of June 2003 and 1st of March 2004
+select * from employee where hiredate between '01-JUN-03' and '01-MAR-04';
 
 --2.7 DELETE
 --Task – Delete a record in Customer table where the name is Robert Walter (There may be constraints that rely on this, find out how to resolve them).
+
+-- find all constraints for a specific table
+--select * from user_cons_columns where table_name = 'INVOICE';
+
+-- disable a specific constraint for a specific table
+--alter table invoice disable constraint fk_invoicecustomerid;
+
+delete customer where firstname = 'Robert' and lastname = 'Walter';
+
 
 --3.0	SQL Functions
 --In this section you will be using the Oracle system functions, as well as your own functions, to perform various actions against the database
 --3.1 System Defined Functions
 --Task – Use a function that returns the current time.
 --Task – Use a function that returns the length of a mediatype from the mediatype table
+
 --3.2 System Defined Aggregate Functions
 --Task – Use a function that returns the average total of all invoices
 --Task – Use a function that returns the most expensive track
+
 --7.0 JOINS
 --In this section you will be working with combing various tables through the use of joins. You will work with outer, inner, right, left, cross, and self joins.
+
 --7.1 INNER
 --Task – Create an inner join that joins customers and orders and specifies the name of the customer and the invoiceId.
+
 --7.2 OUTER
 --Task – Create an outer join that joins the customer and invoice table, specifying the CustomerId, firstname, lastname, invoiceId, and total.
+
 --7.3 RIGHT
 --Task – Create a right join that joins album and artist specifying artist name and title.
+
 --7.4 CROSS
 --Task – Create a cross join that joins album and artist and sorts by artist name in ascending order.
+
 --7.5 SELF
 --Task – Perform a self-join on the employee table, joining on the reportsto column.
 
 
-select * from employee cross join customer;
+--select * from employee cross join customer;
 
 
 
