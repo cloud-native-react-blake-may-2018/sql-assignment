698ce81085ad86804890de364ecc3562a44ef657
crosssh <crosssh@oregonstate.edu>
2018-05-22 17:31:26 -0700

    Finished some of the problems

--- SQL Lab.sql
+++ SQL Lab.sql
@@ -7,24 +7,63 @@
 --Task – Select all records from the Employee table.
     SELECT * FROM employee;
 --Task – Select all records from the Employee table where last name is King.
+    SELECT * FROM employee WHERE lastname like 'King';
 --Task – Select all records from the Employee table where first name is Andrew and REPORTSTO is NULL.
+    SELECT * FROM  employee WHERE firstname LIKE 'Andrew' AND reportsto IS NULL;
 --2.2 ORDER BY
 --Task – Select all albums in Album table and sort result set in descending order by title.
+    SELECT * FROM album ORDER BY title DESC;
 --Task – Select first name from Customer and sort result set in ascending order by city
+    SELECT firstname FROM customer ORDER BY city ASC;
 --2.3 INSERT INTO
 --Task – Insert two new records into Genre table
+    INSERT INTO genre (genreid, name)
+        VALUES (26, 'Cool new genre');
+    INSERT INTO genre (genreid, name)
+        VALUES (27, 'Super cool new genre');
 --Task – Insert two new records into Employee table
+Select * from employee;
+    INSERT INTO employee (employeeid, lastname, firstname, title, reportsto, birthdate, hiredate, address, city, state,country, postalcode, phone, fax, email)
+        VALUES (9, 'Cross', 'Shawn', 'Guy that ', 5, TO_DATE('12-APR-89', 'DD-MON-RR'), TO_DATE('9-OCT-50','DD-MON-RR'), '25095 E Bolton Rd', 'Veneta', 'OR', 'USA', '97448', '+1 (541) 359-5880)', '(541)', 'smcross16@gmail.com'); 
+    INSERT INTO employee (employeeid, lastname, firstname, title, reportsto, birthdate, hiredate, address, city, state,country, postalcode, phone, fax, email)
+        VALUES (10, 'Cross', 'Jim', 'Guy that numero dos', 5, TO_DATE('12-APR-89', 'DD-MON-RR'), TO_DATE('9-OCT-50','DD-MON-RR'), '25095 E Bolton Rd', 'Veneta', 'OR', 'USA', '97448', '+1 (541) 359-5880)', '(541)', 'smcross16@gmail.com');
 --Task – Insert two new records into Customer table
+select * from customer;
+    INSERT INTO customer (customerid, firstname, lastname, company, address, city, state, country, postalcode, phone, fax, email, supportrepid)
+        VALUES (60, 'Jim', 'Jam', 'The Best Company', '123 A St', 'Eugene', 'OR', 'United States', '97402', '+1 541 373 7777', null, 'anemail@anemail.com', 4);
+    INSERT INTO customer (customerid, firstname, lastname, company, address, city, state, country, postalcode, phone, fax, email, supportrepid)
+        VALUES (61, 'Jim', 'Jammin', 'The Worst Company', '453 A St', 'Eugene', 'OR', 'United States', '97402', '+1 541 373 7777', null, 'anemail@anemail.com', 2);
 --2.4 UPDATE
 --Task – Update Aaron Mitchell in Customer table to Robert Walter
+    UPDATE customer 
+    SET firstname = 'Robert', lastname = 'Walter'
+    WHERE firstname = 'Aaron' AND lastname = 'Mitchell';
 --Task – Update name of artist in the Artist table “Creedence Clearwater Revival” to “CCR”
+select * From artist;
+    UPDATE artist 
+    SET name = 'CCR'
+    WHERE name = 'Creedence Clearwater Revival';
 --2.5 LIKE
 --Task – Select all invoices with a billing address like “T%”
+    SELECT * FROM invoice 
+    WHERE billingaddress LIKE 'T%';
 --2.6 BETWEEN
 --Task – Select all invoices that have a total between 15 and 50
+    SELECT * FROM invoice
+    WHERE total BETWEEN 15 AND 50;
 --Task – Select all employees hired between 1st of June 2003 and 1st of March 2004
+    SELECT * FROM employee
+    WHERE hiredate BETWEEN TO_DATE('01-JUN-03', 'DD-MON-RR') AND TO_DATE('01-MAR-04', 'DD-MON-RR');
 --2.7 DELETE
 --Task – Delete a record in Customer table where the name is Robert Walter (There may be constraints that rely on this, find out how to resolve them).
+    DELETE FROM invoice 
+    WHERE customerid = 
+        (SELECT customerid FROM customer 
+         WHERE firstname = 'Robert' AND lastname = 'Walter');
+        
+    
+    DELETE FROM customer 
+    WHERE firstname = 'Robert' AND lastname = 'Walter';
 --
 --3.0	SQL Functions
 --In this section you will be using the Oracle system functions, as well as your own functions, to perform various actions against the database
